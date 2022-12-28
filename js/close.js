2a3d5acdaba2efddf3f7d61375bf79b4d0aef2ae
Zhahongir <reos2001@mail.ru>
2022-12-28 21:45:23 +0300

    final

---
+++ js/close.js
@@ -0,0 +1,20 @@
+$(document).ready(function() { 
+    $('.personal__wrap__close').on('click', function() {
+        $('#first-doctor').fadeOut();
+        $('#second-doctor').fadeOut();
+        $('#third-doctor').fadeOut();
+        $('#four-doctor').fadeOut();
+        $('#five-doctor').fadeOut();
+        $('#six-doctor').fadeOut();
+        $('#seven-doctor').fadeOut();
+        $('#nine-doctor').fadeOut();
+        $('#ten-doctor').fadeOut();
+        $('#fourteen-doctor').fadeOut();
+
+        // $('#eight-doctor').fadeOut();
+        // $('#eleven-doctor').fadeOut();
+        // $('#twelve-doctor').fadeOut();
+        // $('#thirteen-doctor').fadeOut();
+        // $('#ourteen-doctor').fadeOut();
+    });
+});
--- js/modals.js
+++ js/modals.js
@@ -4,7 +4,7 @@ var modalSecond = document.getElementById("second-doctor");
 var modalThird = document.getElementById("third-doctor");
 var modalFour = document.getElementById("four-doctor");
 var modalFive = document.getElementById("five-doctor");
-// var modalSix = document.getElementById("six-doctor");
+var modalSix = document.getElementById("six-doctor");
 var modalSeven = document.getElementById("seven-doctor");
 var modalEight = document.getElementById("eight-doctor");
 var modalNine = document.getElementById("nine-doctor");
@@ -12,13 +12,14 @@ var modalTen = document.getElementById("ten-doctor");
 var modalEleven = document.getElementById("eleven-doctor");
 var modalTwelve = document.getElementById("twelve-doctor");
 var modalThirteen = document.getElementById("thirteen-doctor");
+var modalFourteen = document.getElementById("fourteen-doctor");
 
 var btnFirst = document.getElementById("doctorFirst");
 var btnSecond = document.getElementById("doctorSecond");
 var btnThird = document.getElementById("doctorThird");
 var btnFour = document.getElementById("doctorFour");
 var btnFive = document.getElementById("doctorFive");
-// var btnSix = document.getElementById("doctorSix");
+var btnSix = document.getElementById("doctorSix");
 var btnSeven = document.getElementById("doctorSeven");
 var btnEight = document.getElementById("doctorEight");
 var btnNine = document.getElementById("doctorNine");
@@ -26,13 +27,14 @@ var btnTen = document.getElementById("doctorTen");
 var btnEleven = document.getElementById("doctorEleven");
 var btnTwelve = document.getElementById("doctorTwelve");
 var btnThirteen= document.getElementById("doctorThirteen");
+var btnFourteen= document.getElementById("doctorFourteen");
 
 var closeBtn = document.getElementById("first-doctor-close");
 var closeBtnSecond = document.getElementById("second-doctor-close");
 var closeBtnThird = document.getElementById("third-doctor-close");
 var closeBtnFour = document.getElementById("four-doctor-close");
 var closeBtnFive = document.getElementById("five-doctor-close");
-// var closeBtnSix = document.getElementById("six-doctor-close");
+var closeBtnSix = document.getElementById("six-doctor-close");
 var closeBtnSeven = document.getElementById("seven-doctor-close");
 var closeBtnEight = document.getElementById("eight-doctor-close");
 var closeBtnNine = document.getElementById("nine-doctor-close");
@@ -40,6 +42,7 @@ var closeBtnTen = document.getElementById("ten-doctor-close");
 var closeBtnEleven = document.getElementById("eleven-doctor-close");
 var closeBtnTwelve = document.getElementById("twelve-doctor-close");
 var closeBtnThirteen = document.getElementById("thirteen-doctor-close");
+var closeBtnFourteen = document.getElementById("fourteen-doctor-close");
 
 var bodyOverflow = document.getElementById("bodyBg");
 
@@ -69,20 +72,20 @@ btnFive.onclick = function() {
   bodyOverflow.style.overflowY = "hidden";
 }
 
-// btnSix.onclick = function() {
-//   modalSix.style.display = "block";
-//   bodyOverflow.style.overflowY = "hidden";
-// }
+btnSix.onclick = function() {
+  modalSix.style.display = "block";
+  bodyOverflow.style.overflowY = "hidden";
+}
 
 btnSeven.onclick = function() {
   modalSeven.style.display = "block";
   bodyOverflow.style.overflowY = "hidden";
 }
 
-btnEight.onclick = function() {
-  modalEight.style.display = "block";
-  bodyOverflow.style.overflowY = "hidden";
-}
+// btnEight.onclick = function() {
+//   modalEight.style.display = "block";
+//   bodyOverflow.style.overflowY = "hidden";
+// }
 
 btnNine.onclick = function() {
   modalNine.style.display = "block";
@@ -94,18 +97,23 @@ btnTen.onclick = function() {
   bodyOverflow.style.overflowY = "hidden";
 }
 
-btnEleven.onclick = function() {
-  modalEleven.style.display = "block";
-  bodyOverflow.style.overflowY = "hidden";
-}
+// btnEleven.onclick = function() {
+//   modalEleven.style.display = "block";
+//   bodyOverflow.style.overflowY = "hidden";
+// }
 
-btnTwelve.onclick = function() {
-  modalTwelve.style.display = "block";
-  bodyOverflow.style.overflowY = "hidden";
-}
+// btnTwelve.onclick = function() {
+//   modalTwelve.style.display = "block";
+//   bodyOverflow.style.overflowY = "hidden";
+// }
 
-btnThirteen.onclick = function() {
-  modalThirteen.style.display = "block";
+// btnThirteen.onclick = function() {
+//   modalThirteen.style.display = "block";
+//   bodyOverflow.style.overflowY = "hidden";
+// }
+
+btnFourteen.onclick = function() {
+  modalFourteen.style.display = "block";
   bodyOverflow.style.overflowY = "hidden";
 }
 
@@ -113,7 +121,6 @@ btnThirteen.onclick = function() {
 // Close Buttons
 closeBtn.onclick = function() {
   modal.style.display = "none";
-  bodyOverflow.style.overflowY = "scroll";
 }
 
 closeBtnSecond.onclick = function() {
@@ -146,10 +153,10 @@ closeBtnSeven.onclick = function() {
   bodyOverflow.style.overflowY = "scroll";
 }
 
-closeBtnEight.onclick = function() {
-  modalEight.style.display = "none";
-  bodyOverflow.style.overflowY = "scroll";
-}
+// closeBtnEight.onclick = function() {
+//   modalEight.style.display = "none";
+//   bodyOverflow.style.overflowY = "scroll";
+// }
 
 closeBtnNine.onclick = function() {
   modalNine.style.display = "none";
@@ -161,20 +168,20 @@ closeBtnTen.onclick = function() {
   bodyOverflow.style.overflowY = "scroll";
 }
 
-closeBtnEleven.onclick = function() {
-  modalEleven.style.display = "none";
-  bodyOverflow.style.overflowY = "scroll";
-}
+// closeBtnEleven.onclick = function() {
+//   modalEleven.style.display = "none";
+//   bodyOverflow.style.overflowY = "scroll";
+// }
 
-closeBtnTwelve.onclick = function() {
-  modalTwelve.style.display = "none";
-  bodyOverflow.style.overflowY = "scroll";
-}
+// closeBtnTwelve.onclick = function() {
+//   modalTwelve.style.display = "none";
+//   bodyOverflow.style.overflowY = "scroll";
+// }
 
-closeBtnThirteen.onclick = function() {
-  modalThirteen.style.display = "none";
-  bodyOverflow.style.overflowY = "scroll";
-}
+// closeBtnThirteen.onclick = function() {
+//   modalThirteen.style.display = "none";
+//   bodyOverflow.style.overflowY = "scroll";
+// }
 
 // window.onclick = function(event) {
 //   if (event.target == modal) {
@@ -182,6 +189,11 @@ closeBtnThirteen.onclick = function() {
 //     bodyOverflow.style.overflowY = "scroll";
 //   }
 // }
+
+closeBtnFourteen.onclick = function() {
+  modalFourteen.style.display = "none";
+  bodyOverflow.style.overflowY = "scroll";
+}
 
 
 
